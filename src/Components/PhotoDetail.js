97a65f91007d7a9e00b072635d50b6b0d116ca67
Jeffrey Webb <jeff@MacBook-Pro.local>
2018-08-22 23:30:10 -0400

    constructor error

--- src/Components/PhotoDetail.js
+++ src/Components/PhotoDetail.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
 
+import data from '.Components/data'
 
 
 class PhotoDetail extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            list:[]
+        }
+    }
+
+    componentDidMount() {
+        this.setState({ list:data})
+    }
+
     render() {
         return (
             <div>
-               Photo Detail 
+                {this.state.list.map((pandas, i) => {
+                    return <li>
+                        {pandas.title}
+                        <img src={"https://cdn-images-1.medium.com/max/1600/1*i1vVm3EqqDIkyucD0079wg.jpeg"} />
+                    </li>
+                )})
+                
+                
             </div>
         );
     }
