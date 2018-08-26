5fa0e28abba7a3b76f3687094f616692441e6c52
Jeffrey Webb <jeff@MacBook-Pro.local>
2018-08-26 09:38:59 -0400

    fixed photodetails

--- src/Components/PhotoDetail.js
+++ src/Components/PhotoDetail.js
@@ -1,23 +1,20 @@
 import React, { Component } from 'react';
 
-import data from '../Data/hobbies.json'
+import hobbies from '../Data/hobbies.json'
 
 class PhotoDetail extends Component {
     render() {
-        let photo = data[this.props.match.params.category].photos
-        let id = this.props.match.params.id
+        const _category = this.props.match.params.category;
+        const _index = this.props.match.params.index;
+        const _photoData = hobbies[_category].photos[_index]
+
         return (
-            <div className="photo-card">
-                <div className="photo-title">
-                    <h3>{photo[id].title}</h3>
-                </div>
-                <div className="photo">
-                    img src={photo[id].imageURL} alt={photo[id].title} />
-                    </div>
-                    <div className="photo-source">
-                        <a href={photo[id].sourceURL}>Source</a>
-                    </div>
-                </div>   
+            <div>
+            <header>{_photoData.title}</header>
+            <img src={_photoData.imageURL} alt={_photoData.title} />
+            <a href={_photoData.sourceURL}>Source</a>
+
+            </div>  
         );
     }
 }
